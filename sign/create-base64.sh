738c8c818709b314ff5b43a7c31f1dd2b37b13c8
Milan Jurkulak <mimoccc@gmail.com>
2025-07-20 13:18:43 +0200

    build fixes

--- sign/create-base64.sh
+++ sign/create-base64.sh
@@ -4,22 +4,22 @@ openssl base64 < "$signing_name.jks" | tr -d '\n' > "$signing_name.jks.base64.tx
 # define github secrets
 # SIGNING_KEY_BASE64, KEYSTORE_PASSWORD, KEY_PASSWORD, KEY_ALIAS
 # steps:
-#  - uses: r0adkll/sign-android-release@v1
+#  - uses: ilharp/sign-android-release@v2.0.0
 #    name: Sign app APK
 #    # ID used to access action output
 #    id: sign_app
 #    with:
-#      releaseDirectory: app/build/outputs/apk/release
+#      releaseDirectory: composeApp/build/outputs/apk/release
 #      signingKeyBase64: ${{ secrets.SIGNING_KEY }}
 #      alias: ${{ secrets.ALIAS }}
 #      keyStorePassword: ${{ secrets.KEY_STORE_PASSWORD }}
 #      keyPassword: ${{ secrets.KEY_PASSWORD }}
 #    env:
-#      # override default build-tools version (33.0.0) -- optional
-#      # BUILD_TOOLS_VERSION: "34.0.0"
+#      # override default build-tools version (36.0.0) -- optional
+#      # BUILD_TOOLS_VERSION: "36.0.0"
 #
-#  # Example use of `signedReleaseFile` output -- not needed
+#  # Example use of `signedFile` output -- not needed
 #  - uses: actions/upload-artifact@v2
 #    with:
 #      name: Signed app bundle
-#      path: ${{steps.sign_app.outputs.signedReleaseFile}}
+#      path: ${{steps.sign_app.outputs.signedFile}}
