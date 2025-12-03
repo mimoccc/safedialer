f93b59eba675e2c00d611d33f910a4e6fa7de6bb
Milan Jurkulak <mimoccc@gmail.com>
2025-12-03 10:54:17 +0100

    buttons visibility state

--- composeApp/src/androidMain/kotlin/org/mjdev/safedialer/ui/components/ContactButtonsDefault.kt
+++ composeApp/src/androidMain/kotlin/org/mjdev/safedialer/ui/components/ContactButtonsDefault.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import android.provider.ContactsContract
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -19,6 +20,8 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
@@ -28,114 +31,118 @@ import org.mjdev.safedialer.data.list.ListItem
 import org.mjdev.safedialer.extensions.CustomExt.isInPreviewMode
 import org.mjdev.safedialer.helpers.Previews
 
+// todo visible state
 @Previews
 @Composable
 fun ContactButtonsDefault(
     item: ListItem? = if (isInPreviewMode) ListItem.PREVIEW else null,
+    state: MutableState<Boolean> = mutableStateOf(false),
     iconSize: Dp = 32.dp,
     context: Context = LocalContext.current,
-) = Row {
-    if (item?.itemPhone?.isNotEmpty() == true || isInPreviewMode) {
-        IconButton(
-            modifier = Modifier.size(iconSize),
-            onClick = {
-                val intent = Intent(
-                    Intent.ACTION_DIAL,
-                    Uri.parse("tel:${item?.itemPhone}")
-                ).apply {
-                    addFlags(FLAG_ACTIVITY_NEW_TASK)
-                }
-                context.startActivity(intent)
-            },
-        ) {
-            Image(
-                imageVector = Icons.Filled.Call,
-                contentDescription = "",
-                modifier = Modifier.padding(4.dp),
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-        IconButton(
-            modifier = Modifier.size(iconSize),
-            onClick = {
-                val intent = Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse("sms:${item?.itemPhone}")
-                ).apply {
-                    addFlags(FLAG_ACTIVITY_NEW_TASK)
-                }
-                context.startActivity(intent)
-            }
-        ) {
-            Image(
-                imageVector = Icons.Filled.Email,
-                contentDescription = "",
-                modifier = Modifier.padding(4.dp),
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-        IconButton(
-            modifier = Modifier.size(iconSize),
-            onClick = {
-                // todo block
-            }
-        ) {
-            Image(
-                imageVector = if (item?.isBlocked == true) Icons.Filled.FavoriteBorder
-                else Icons.Filled.Favorite,
-                contentDescription = "",
-                modifier = Modifier.padding(4.dp),
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    }
-    if (item?.itemId != null) {
-        IconButton(
-            modifier = Modifier.size(iconSize),
-            onClick = {
-                val intent = Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.withAppendedPath(
-                        ContactsContract.Contacts.CONTENT_URI,
-                        item.itemId.toString()
+) = AnimatedVisibility(visible = state.value) {
+    Row {
+        if (item?.itemPhone?.isNotEmpty() == true || isInPreviewMode) {
+            IconButton(
+                modifier = Modifier.size(iconSize),
+                onClick = {
+                    val intent = Intent(
+                        Intent.ACTION_DIAL,
+                        Uri.parse("tel:${item?.itemPhone}")
+                    ).apply {
+                        addFlags(FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    context.startActivity(intent)
+                },
+            ) {
+                Image(
+                    imageVector = Icons.Filled.Call,
+                    contentDescription = "",
+                    modifier = Modifier.padding(4.dp),
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
                     )
-                ).apply {
-                    addFlags(FLAG_ACTIVITY_NEW_TASK)
+                )
+            }
+            IconButton(
+                modifier = Modifier.size(iconSize),
+                onClick = {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("sms:${item?.itemPhone}")
+                    ).apply {
+                        addFlags(FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    context.startActivity(intent)
                 }
-                context.startActivity(intent)
-            }
-        ) {
-            Image(
-                imageVector = Icons.Filled.ContactPhone,
-                contentDescription = "",
-                modifier = Modifier.padding(4.dp),
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.primary
+            ) {
+                Image(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = "",
+                    modifier = Modifier.padding(4.dp),
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
+                    )
                 )
-            )
+            }
+            IconButton(
+                modifier = Modifier.size(iconSize),
+                onClick = {
+                    // todo block
+                }
+            ) {
+                Image(
+                    imageVector = if (item?.isBlocked == true) Icons.Filled.FavoriteBorder
+                    else Icons.Filled.Favorite,
+                    contentDescription = "",
+                    modifier = Modifier.padding(4.dp),
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
         }
-    }
-    if (item?.itemId != null) {
-        IconButton(
-            modifier = Modifier.size(iconSize),
-            onClick = {
-                // todo : delete
-            }
-        ) {
-            Image(
-                imageVector = Icons.Filled.Delete,
-                contentDescription = "",
-                modifier = Modifier.padding(4.dp),
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.primary
+        if (item?.itemId != null) {
+            IconButton(
+                modifier = Modifier.size(iconSize),
+                onClick = {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.withAppendedPath(
+                            ContactsContract.Contacts.CONTENT_URI,
+                            item.itemId.toString()
+                        )
+                    ).apply {
+                        addFlags(FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    context.startActivity(intent)
+                }
+            ) {
+                Image(
+                    imageVector = Icons.Filled.ContactPhone,
+                    contentDescription = "",
+                    modifier = Modifier.padding(4.dp),
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
+                    )
                 )
-            )
+            }
+        }
+        if (item?.itemId != null) {
+            IconButton(
+                modifier = Modifier.size(iconSize),
+                onClick = {
+                    // todo : delete
+                }
+            ) {
+                Image(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "",
+                    modifier = Modifier.padding(4.dp),
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
         }
     }
 }
