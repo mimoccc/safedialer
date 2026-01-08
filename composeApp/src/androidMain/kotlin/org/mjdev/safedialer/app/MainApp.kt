1a591e4cdd86b21fbbe561a19b77c7111c232def
Milan Jurkulak <mimoccc@gmail.com>
2026-01-08 12:52:31 +0100

    crash handler

--- composeApp/src/androidMain/kotlin/org/mjdev/safedialer/app/MainApp.kt
+++ composeApp/src/androidMain/kotlin/org/mjdev/safedialer/app/MainApp.kt
@@ -1,8 +1,8 @@
 package org.mjdev.safedialer.app
 
 import android.app.Application
+import android.util.Log
 import androidx.core.telecom.CallsManager
-//import kotbase.CouchbaseLite
 import org.kodein.di.DIAware
 import org.kodein.di.LazyDI
 import org.kodein.di.instance
@@ -19,8 +19,24 @@ class MainApp : Application(), DIAware {
     override fun onCreate() {
         callsManager.registerAppWithTelecom(capabilities)
         super.onCreate()
+        setupExceptionHandler()
         // todo check if from boot & permissions granted & permission activity
         IncomingCallService.start(this)
         MediaService.start(this)
+    }
+
+    private fun setupExceptionHandler() {
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e(TAG, "$TAG_CRASH: Uncaught exception in thread: ${thread.name}", throwable)
+            Log.e(TAG, "$TAG_CRASH: Exception: ${throwable.javaClass.name}: ${throwable.message}")
+            Log.e(TAG, "$TAG_CRASH: Stack trace:\n${throwable.stackTraceToString()}")
+            defaultHandler?.uncaughtException(thread, throwable)
+        }
+    }
+
+    companion object {
+        private val TAG = MainApp::class.simpleName
+        private const val TAG_CRASH = "Crash"
     }
 }
